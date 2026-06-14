5174351e9aab17e46998d38ed46d45b6712a4e30
Akash Biswas <akashbiswas.271999@gmail.com>
2026-06-14 10:57:42 +0530

    Create handlers to recieve the requests.

--- services/byte-processor/src/main.rs
+++ services/byte-processor/src/main.rs
@@ -1,8 +1,21 @@
-mod byte_processor_grpc;
+use nimbus_object_service::{
+    ByteProcessorService, nimbus_proto::nimbus_object_service_server::NimbusObjectServiceServer,
+};
 
-use byte_processor_grpc::handler;
+mod nimbus_object_service;
+use tonic::transport::Server;
 
-fn main() {
-    let str = handler();
-    println!("{str}");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let byte_processor_service_impl = ByteProcessorService::default();
+
+    println!("gRPC Server listening on {}", addr);
+
+    Server::builder()
+        .add_service(NimbusObjectServiceServer::new(byte_processor_service_impl))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
---
+++ services/byte-processor/src/nimbus_object_service/mod.rs
@@ -0,0 +1,64 @@
+use nimbus_proto::{
+    CreateOrUpdateObjectRequest, CreateOrUpdateObjectResponse, GetObjectRequest, GetObjectResponse,
+    nimbus_object_service_server::NimbusObjectService,
+};
+use std::pin::Pin;
+use tokio::sync::mpsc;
+use tokio_stream::{Stream, wrappers::ReceiverStream};
+use tonic::{Request, Response, Status, Streaming};
+
+pub mod nimbus_proto {
+    pub mod types {
+        pub mod v1 {
+            include!("../proto-gen/nimbus/v1/types/v1/nimbus.v1.types.v1.rs");
+        }
+    }
+
+    include!("../proto-gen/nimbus/v1/nimbus.v1.rs");
+}
+#[derive(Default, Debug)]
+pub struct ByteProcessorService {}
+
+#[tonic::async_trait]
+impl NimbusObjectService for ByteProcessorService {
+    // Define the type signature for our response stream
+    type GetObjectStream = Pin<Box<dyn Stream<Item = Result<GetObjectResponse, Status>> + Send>>;
+    async fn create_or_update_object(
+        &self,
+        request: Request<Streaming<CreateOrUpdateObjectRequest>>,
+    ) -> std::result::Result<Response<CreateOrUpdateObjectResponse>, Status> {
+        let mut _stream = request.into_inner();
+        let data = CreateOrUpdateObjectResponse::default();
+        Ok(Response::from(data))
+    }
+
+    async fn get_object(
+        &self,
+        request: Request<GetObjectRequest>,
+    ) -> Result<Response<Self::GetObjectStream>, tonic::Status> {
+        let _id = request.into_inner().object_id;
+
+        let (tx, rx) = mpsc::channel(10);
+
+        tokio::spawn(async move {
+            for i in 1..00 {
+                let chunk = GetObjectResponse {
+                    data: format!("Hello {i}").into_bytes(),
+                };
+
+                // Send the message to the channel. If the client disconnects,
+                // tx.send() will error, and we break early to clean up resources.
+                if tx.send(Ok(chunk)).await.is_err() {
+                    eprintln!("Client disconnected early.");
+                    break;
+                }
+            }
+        });
+
+        let output_stream = ReceiverStream::new(rx);
+
+        Ok(Response::from(
+            Box::pin(output_stream) as Self::GetObjectStream
+        ))
+    }
+}
