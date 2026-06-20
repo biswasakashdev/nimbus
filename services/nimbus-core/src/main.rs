69d6a56a5cd1779ff548f5131b798fe8088c5328
Akash Biswas <akashbiswas.271999@gmail.com>
2026-06-20 21:38:02 +0530

    Add config for scylladb in docker-compose and application also.

--- services/nimbus-core/src/main.rs
+++ services/nimbus-core/src/main.rs
@@ -7,6 +7,7 @@ use nimbus_object_service::{
     NimbusCoreService, nimbus_proto::nimbus_public_service_server::NimbusPublicServiceServer,
 };
 
+use scylla::client::session_builder::SessionBuilder;
 use tonic::transport::Server;
 
 use crate::local_storage::LocalStorageCluster;
@@ -14,7 +15,12 @@ use crate::local_storage::LocalStorageCluster;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50052".parse()?;
+    let scylla_url = "127.0.0.1:9042";
     let storage = NimbusCoreService::new(LocalStorageCluster::new());
+
+    let _scylla_session = SessionBuilder::new().known_node(scylla_url).build().await?;
+
+    println!("Connected to scylla DB.");
 
     println!("gRPC Server listening on {}", addr);
 
--- services/nimbus-core/src/storage_cluster/mod.rs
+++ services/nimbus-core/src/storage_cluster/mod.rs
@@ -1,3 +1,13 @@
+use scylla::client::{session::Session, session_builder::SessionBuilder};
+
 use crate::storage::Storage;
 
-pub struct StorageCluster {}
+pub struct StorageCluster {
+    session: Session,
+}
+
+impl StorageCluster {
+    fn new(session: Session) -> Self {
+        Self { session: session }
+    }
+}
