ae91b46a1537710a7a9a874948f3a081e3edc71f
Akash Biswas <akashbiswas.271999@gmail.com>
2026-06-27 13:17:54 +0530

    Implement the heartbeat sender.

--- services/nimbus-core/src/main.rs
+++ services/nimbus-core/src/main.rs
@@ -1,20 +1,26 @@
+pub mod event_streamer;
 pub mod local_storage;
 pub mod nimbus_object_service;
 pub mod proto_gen;
 pub mod storage;
 pub mod storage_cluster;
 
-use proto_gen::nimbus_public::v1::nimbus_public_service_server::NimbusPublicServiceServer;
+use proto_gen::{
+    nimbus_public::v1::nimbus_public_service_server::NimbusPublicServiceServer,
+    service_discovery::v1::nimbus_core_service_discovery_service_client::NimbusCoreServiceDiscoveryServiceClient,
+};
 
 use nimbus_object_service::NimbusCoreService;
 
-use scylla::client::session_builder::SessionBuilder;
+use scylla::client::{self, session_builder::SessionBuilder};
 use tonic::transport::Server;
 
 use crate::local_storage::LocalStorageCluster;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tokio::spawn(async {});
+
     let addr = "[::1]:50052".parse()?;
     let scylla_url = "127.0.0.1:9042";
     let storage = NimbusCoreService::new(LocalStorageCluster::new());
